4e8a221fc8846afabcbe6162b035dcec09050899
decorgraphicsstudio <hasanraza03165825495@gmail.com>
2025-12-23 16:14:55 +0500

    .

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -14,7 +14,7 @@ const Loader: React.FC = () => {
     >
       <div className="relative flex flex-col items-center gap-12">
         {/* Pulsing Emblem */}
-        <motion.div
+        {/* <motion.div
           initial={{ scale: 0.8, opacity: 0 }}
           animate={{ 
             scale: [0.8, 1.1, 1],
@@ -28,27 +28,25 @@ const Loader: React.FC = () => {
           className="w-24 h-24 rounded-full border-2 border-[#FDCD07] flex items-center justify-center font-serif text-[#FDCD07] font-black text-2xl shadow-[0_0_40px_rgba(253,205,7,0.2)]"
         >
           KM
-        </motion.div>
+        </motion.div> */}
 
-        {/* Brand Text Staggered */}
-        <div className="overflow-hidden flex flex-col items-center">
-          <motion.h2 
-            initial={{ y: 40 }}
-            animate={{ y: 0 }}
-            transition={{ duration: 0.8, ease: "easeOut" }}
-            className="text-white font-serif text-2xl uppercase tracking-[0.4em] font-black"
-          >
-            Khatti Methi
-          </motion.h2>
-          <motion.p 
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ delay: 0.4 }}
-            className="text-[#FDCD07] text-[10px] uppercase tracking-[0.6em] font-bold mt-2"
-          >
-            Imperial Dining
-          </motion.p>
-        </div>
+        {/* Pulsing Emblem */}
+        <motion.div
+          animate={{ 
+            scale: [1, 1.1, 1],
+            opacity: 1 
+          }}
+          transition={{ 
+            duration: 1.5,
+            repeat: Infinity,
+            repeatType: "reverse"
+          }}
+          className=""
+        >
+          <div className="logo h-14">
+              <img src="logo.png" alt="KM" className="w-full h-full object-contain"/>
+          </div>
+        </motion.div>
 
         {/* Rounded Progress Bar */}
         <div className="w-48 h-[2px] bg-white/10 rounded-full overflow-hidden relative">
@@ -58,7 +56,6 @@ const Loader: React.FC = () => {
             transition={{ 
               duration: 2, 
               ease: "easeInOut",
-              repeat: Infinity
             }}
             className="absolute inset-0 bg-[#FDCD07] shadow-[0_0_10px_#FDCD07]"
           />
